623d61553876140100a323aeca88a836d70246c4
Tejas K <atejask2017@gmail.com>
2022-07-14 02:32:43 +0530

    Frontend and Backend Done, Connected to Database, WebApp ready

--- TodoApp/todos/models.py
+++ TodoApp/todos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
 # Create your models here.
-class ToDo(models.Model): #creates table ToDo
+
+class Todo(models.Model): #creates table ToDo
     content = models.TextField() #creates column named content
